6b3d1f159bff613e67bb8794482b385d65c17580
ywc2016 <yu_weicheng@buaa.edu.cn>
2017-03-11 21:16:26 +0800

    1

--- jiami/src/com/ywc/test/TestCoder.java
+++ jiami/src/com/ywc/test/TestCoder.java
@@ -8,6 +8,7 @@ import org.junit.Test;
 
 import com.ywc.base64.Coder;
 import com.ywc.des.DESCoder;
+import com.ywc.pbe.PBECoder;
 
 public class TestCoder {
 	@Test
@@ -69,6 +70,28 @@ public class TestCoder {
 
 		System.err.println("解密后:\t" + outputStr);
 
+		assertEquals(inputStr, outputStr);
+	}
+
+	@Test
+	public void testPBECoder() throws Exception {
+		String inputStr = "abc";
+		System.err.println("原文: " + inputStr);
+		byte[] input = inputStr.getBytes();
+
+		String pwd = "efg";
+		System.err.println("密码: " + pwd);
+
+		byte[] salt = PBECoder.initSalt();
+
+		byte[] data = PBECoder.encrypt(input, pwd, salt);
+
+		System.err.println("加密后: " + PBECoder.encryptBASE64(data));
+
+		byte[] output = PBECoder.decrypt(data, pwd, salt);
+		String outputStr = new String(output);
+
+		System.err.println("解密后: " + outputStr);
 		assertEquals(inputStr, outputStr);
 	}
 }
