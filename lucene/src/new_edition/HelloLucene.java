cdc03a483d98aada748b3a83a57d119650f082dd
ywc2016 <yu_weicheng@buaa.edu.cn>
2017-03-12 10:10:13 +0800

    1

---
+++ lucene/src/new_edition/HelloLucene.java
@@ -0,0 +1,76 @@
+package new_edition;
+
+public class HelloLucene {
+
+//	public void index() {
+//		// 1.创建Director
+//		// Directory directory = new RAMDirectory();// 创建在内存
+//		Directory directory = null;
+//		try {
+//			directory = FSDirectory.open(Paths.get("directory"));// 创建在硬盘
+//		} catch (IOException e1) {
+//			e1.printStackTrace();
+//		}
+//
+//		IndexWriter indexWriter = null;
+//		IndexWriterConfig indexWriterConfig = new IndexWriterConfig(new StandardAnalyzer());
+//		try {
+//			// 2.创建IndexWriter
+//			indexWriter = new IndexWriter(directory, indexWriterConfig);
+//
+//			File dir = new File("txt");
+//			for (File file : dir.listFiles()) {
+//				// 3.创建Document
+//				Document document = new Document();
+//				// 4.为Document添加Field
+//				document.add(new Field("content", new FileReader(file), TextField.TYPE_NOT_STORED));
+//				document.add(new Field("fileName", file.getName(), TextField.TYPE_STORED));
+//				document.add(new Field("path", file.getAbsolutePath(), TextField.TYPE_STORED));
+//				// 添加文档到索引
+//				indexWriter.addDocument(document);
+//			}
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
+//		try {
+//			indexWriter.close();
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
+//	}
+//
+//	public void search(String keyWord) {
+//
+//		try {
+//			// 1.创建Directory
+//			// 2.创建IndexReader
+//			Directory directory = FSDirectory.open(Paths.get("directory"));// 创建在硬盘
+//			DirectoryReader directoryReader = DirectoryReader.open(directory);
+//			// 3.创建IndexSearcher
+//			IndexSearcher indexSearcher = new IndexSearcher(directoryReader);
+//			// 4.创建搜索的Query
+//			QueryParser queryParser = new QueryParser("content", new StandardAnalyzer());
+//			Query query = queryParser.parse(keyWord);
+//
+//			// 5、根据searcher搜索并且返回TopDocs
+//			TopDocs topDocs = indexSearcher.search(query, 10);
+//			System.out.println("查找到的文档总共有：" + topDocs.totalHits);
+//			// 6、根据TopDocs获取ScoreDoc对象
+//			ScoreDoc[] scoreDocs = topDocs.scoreDocs;
+//			for (ScoreDoc scoreDoc : scoreDocs) {
+//
+//				// 7、根据searcher和ScoreDoc对象获取具体的Document对象
+//				Document document = indexSearcher.doc(scoreDoc.doc);
+//
+//				// 8、根据Document对象获取需要的值
+//				System.out.println(document.get("fileName") + " " + document.get("path"));
+//			}
+//			directoryReader.close();
+//		} catch (ParseException e) {
+//			e.printStackTrace();
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
+//
+//	}
+}
--- lucene/src/test/MyTest.java
+++ lucene/src/test/MyTest.java
@@ -1,28 +1,18 @@
 package test;
 
-import org.apache.lucene.queryparser.classic.ParseException;
 import org.junit.Test;
 
-import lucene.HelloLucene;
+import new_edition.HelloLucene;
 
 public class MyTest {
-	@Test
-	public void testIndex() {
-		new HelloLucene().index();
-	}
+//	@Test
+//	public void testIndex() {
+//		new HelloLucene().index();
+//	}
+//
+//	@Test
+//	public void testSearch() {
+//		new HelloLucene().search("import");
+//	}
 
-	@Test
-	public void testSearch() throws ParseException {
-		new HelloLucene().search("北航");
-	}
-
-	@Test
-	public void testQueryNumOfDocs() {
-		new HelloLucene().queryNumOfDocs();
-	}
-
-	@Test
-	public void testDeleteDocs() {
-		new HelloLucene().deleteDocuments();
-	}
 }
