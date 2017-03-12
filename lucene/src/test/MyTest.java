3a75a68f06d9bdb2d61197136d452408efec381c
ywc2016 <yu_weicheng@buaa.edu.cn>
2017-03-12 15:27:41 +0800

    1

---
+++ lucene/src/test/MyTest.java
@@ -0,0 +1,18 @@
+package test;
+
+import org.junit.Test;
+
+import new_edition.HelloLucene;
+
+public class MyTest {
+//	@Test
+//	public void testIndex() {
+//		new HelloLucene().index();
+//	}
+//
+//	@Test
+//	public void testSearch() {
+//		new HelloLucene().search("import");
+//	}
+
+}
